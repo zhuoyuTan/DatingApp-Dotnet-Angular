2b9a0b6117a35807621bd7dd77f068c414794d45
zhuoyu tan <zhuoyut96@gmail.com>
2022-07-13 23:56:03 -0700

    sectoin 8(update): bug fix

---
+++ API/Data/Migrations/20220714065340_editPhotoTableName.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class editPhotoTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photo_Users_AppUserId",
+                table: "Photo");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Photo",
+                table: "Photo");
+
+            migrationBuilder.RenameTable(
+                name: "Photo",
+                newName: "Photos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Photo_AppUserId",
+                table: "Photos",
+                newName: "IX_Photos_AppUserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Photos",
+                table: "Photos",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Users_AppUserId",
+                table: "Photos",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Users_AppUserId",
+                table: "Photos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Photos",
+                table: "Photos");
+
+            migrationBuilder.RenameTable(
+                name: "Photos",
+                newName: "Photo");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photo",
+                newName: "IX_Photo_AppUserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Photo",
+                table: "Photo",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photo_Users_AppUserId",
+                table: "Photo",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
